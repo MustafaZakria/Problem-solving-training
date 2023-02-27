e29f05ba974775ca8f50cc84150112c969d8caf3
Mustafa Zakria <mustafa.zakria21@gmail.com>
2023-02-27 20:49:21 +0200

    Solved search problems

---
+++ Level-2/src/HackerRank/algorithmsbasics/search/IcecreamParlor.java
@@ -0,0 +1,71 @@
+package HackerRank.algorithmsbasics.search;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result1 {
+
+    /*
+     * Complete the 'icecreamParlor' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER m
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
+        int index1 = 0, index2 = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            index1 = i;
+            int temp = m - arr.get(index1);
+            arr.set(index1, -1);
+            if (arr.contains(temp) && temp > 0) {
+                index2 = arr.indexOf(temp);
+                break;
+            }
+        }
+        return Arrays.asList(index1 + 1, index2 + 1);
+    }
+
+}
+
+public class IcecreamParlor {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                List<Integer> result = Result1.icecreamParlor(m, arr);
+
+                bufferedWriter.write(
+                        result.stream()
+                                .map(Object::toString)
+                                .collect(joining(" "))
+                                + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Level-2/src/HackerRank/algorithmsbasics/search/SherlockAndArray.java
@@ -0,0 +1,71 @@
+package HackerRank.algorithmsbasics.search;
+
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+        import static java.util.stream.Collectors.toList;
+
+class Result3 {
+
+    /*
+     * Complete the 'balancedSums' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static String balancedSums(List<Integer> arr) {
+        int sum1 = 0,
+                sum2 = 0;
+
+        for(int i = 1; i < arr.size(); i++)
+            sum2 += arr.get(i);
+
+        for(int i = 0; i < arr.size(); i++) {
+            if(sum1 - sum2 == 0)
+                return "YES";
+
+            sum1 += arr.get(i);
+            if(i < arr.size() - 1)
+                sum2 -= arr.get(i + 1);
+        }
+        return "NO";
+    }
+}
+
+public class SherlockAndArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                String result = Result3.balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
